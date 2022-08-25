a8b7003cd6a2721c6ba3e768544fdb8c20ca65e0
David Leiter <davidaleiter@gmail.com>
2022-08-25 14:36:25 -0600

    Created component

---
+++ YtApi/src/app/components/thumbnail-card/thumbnail-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail-card',
+  templateUrl: './thumbnail-card.component.html',
+  styleUrls: ['./thumbnail-card.component.scss']
+})
+export class ThumbnailCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
