fcdd312b8070497249c94ffed9f63b68e8427c5b
David Leiter <davidaleiter@gmail.com>
2022-08-30 11:22:13 -0600

    moved angular materials to separate module. Added stylings for bootstrap

--- YtApi/src/app/app.module.ts
+++ YtApi/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCommonModule } from '@angular/material/core';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
+import { MaterialModule } from './material.module';
+
 
 import { YouTubeAuthService, UserInfo } from './services/youtube-auth.service';
-
-import { AppComponent } from './app.component';
 import { YoutubeApiService } from './services/youtube-api.service';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { ThumbnailCardComponent } from './components/thumbnail-card/thumbnail-card.component';
 
 @NgModule({
@@ -30,18 +24,12 @@ import { ThumbnailCardComponent } from './components/thumbnail-card/thumbnail-ca
   ],
 
   imports: [
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSliderModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatCommonModule,
-    MatGridListModule,
-    HttpClientModule,
-    CommonModule,
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    MaterialModule,
     OAuthModule.forRoot(),
   ],
 
